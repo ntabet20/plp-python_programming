a4f3576870102cab43951148772dfc93b71e00ef
ntabet20 <ntabetsalomon@gmail.com>
2025-08-07 01:21:03 +0100

    Week2 assigment on Python built-in Data Structures

---
+++ week2.py
@@ -0,0 +1,26 @@
+# Week2 Assigment
+# Python Data Structures
+
+# 1. Create empty list
+my_list = []
+
+# 2. Append items in list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# 3. Insert 15 at second position
+my_list.insert(1, 15)
+
+# 4. Extend with another list
+my_list.extend([50, 60, 70])
+
+# 5. Remove last element
+my_list.pop(len(my_list) - 1)
+
+# 6. Sorting in ascending order
+my_list.sort()
+
+# 7. Find and print index of 30
+print(my_list.index(30))
